c9355e1988a7973f9d4c83634bbfc2a0a1191cba
BiltosLab <mrlaith44@gmail.com>
2023-09-07 19:01:17 +0300

    Changes to be committed: 	modified:   Makefile 	modified:   bin/boot.bin 	new file:   bin/kernel.bin 	new file:   bin/os.bin 	new file:   build/kernel.asm.o 	new file:   build/kernelfull.o 	modified:   src/boot/boot.asm 	new file:   src/kernel.asm 	new file:   src/linker.ld

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -29,7 +29,7 @@ step2:
     or eax, 0x1
     mov cr0, eax
     jmp CODE_SEG:load32
-
+ 
 ;GDT
 gdt_start:
 gdt_null:
@@ -58,21 +58,68 @@ gdt_end:
 gdt_descriptor:
     dw gdt_end - gdt_start-1
     dd gdt_start
+ [BITS 32]
+ load32:
+    mov eax, 1
+    mov ecx, 100
+    mov edi, 0x0100000
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
+    
+ata_lba_read:
+    mov ebx, eax ;Backup the LBA
+    ; Send the highest 8 bits of the lba to harddisk controller
+    shr eax, 24
+    or eax, 0xE0 ;Select the master drive
+    mov dx, 0x1F6
+    out dx, al
+    ;Finished sending the highest 8 bits of the lba
 
-[BITS 32]
-load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax 
-    mov ebp, 0x00200000
-    mov esp, ebp
-    in al, 0x92 ; Enable the A20 line using the Fast A20 Gate
-    or al, 2
-    out 0x92, al
-    jmp $
+    ;Send the total sectors to read
+    mov eax, ecx
+    mov dx, 0x1F2
+    out dx, al
+    ;Finished sending the total sectors to read
+    ;Send more bits of the LBA
+    mov eax, ebx ;Restore the backup of LBA
+    mov dx, 0x1F3
+    out dx, al
+    ;Finished sending more bits of the LBA
+    ;Send more bits of the LBA
+    mov dx, 0x1F4
+    mov eax, ebx ;Restore the backup of LBA
+    shr eax, 8
+    out dx, al
+    ;Finished sending more bits of the LBA
+    
+    ;Send upper 16 bits of the LBA 
+    mov dx, 0x1F5
+    mov eax, ebx ;Restore the backup of LBA
+    shr eax, 16
+    out dx, al
+    ;Finished sending upper 16 bits of the LBA
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+.next_sector:
+    push ecx
+
+.try_again:
+    mov dx, 0x1f7
+    in al, dx
+    test al, 8
+    jz .try_again
+
+;We need to read 256 words at a time
+    mov ecx, 256
+    mov dx, 0x1F0
+    rep insw
+    pop ecx
+    loop .next_sector
+    ;End of reading sectors into memory
+    ret
 
 times 510-($ - $$) db 0
 dw 0xAA55
