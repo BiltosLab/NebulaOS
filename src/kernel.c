9073d5c578ef7fb8ed8e7783126bbb8fc2dbdd02
BiltosLab <mrlaith44@gmail.com>
2023-10-02 01:07:08 +0300

    Added functions to change the pages and where they point

--- src/kernel.c
+++ src/kernel.c
@@ -83,6 +83,7 @@ void print(const char* str){
     
 }
 
+
 static struct paging_4gb_chunk* kernel_chunk = 0;
 void kernel_main()
 {
@@ -100,8 +101,17 @@ void kernel_main()
     
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
+    char* ptr = kzalloc(4096);
+    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr |PAGING_ACCESS_FROM_ALL|PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
 
+   
     enable_paging();
+
+    char* ptr2 = (char*) 0x1000; // testing paging 
+    ptr2[0]='A';
+    ptr2[1]='B';
+    print(ptr2);
+    print(ptr);
     //Enable the system interrupts
     enable_interrupts();
 
--- src/memory/paging/paging.h
+++ src/memory/paging/paging.h
@@ -2,6 +2,7 @@
 #define PAGING_H
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 
 #define PAGING_CACHE_DISABLED  0b00010000
@@ -25,5 +26,11 @@ struct paging_4gb_chunk* paging_new_4gb(uint8_t flags);
 void paging_switch(uint32_t* directory);
 void enable_paging();
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk);
+int paging_set(uint32_t* directory,void* virt,uint32_t val);
+bool paging_is_aligned(void* addr);
+
+
+
+
 
 #endif
